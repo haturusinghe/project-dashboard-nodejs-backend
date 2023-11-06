648306807587b48dad23dbcbdbc8f26456bc47ab
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-06 18:55:16 +0530

    working state

--- controller/ProjectController.js
+++ controller/ProjectController.js
@@ -6,21 +6,14 @@ function sendResponse(res, status, data) {
 }
 
 function getAllProjects(req, res) {
+  console.log("getting all projects");
   sendResponse(res, 200, projects);
 }
 
-function getProject(req, res, id) {
-  const project = projects.find((p) => p.id === id);
-  if (project) {
-    sendResponse(res, 200, project);
-  } else {
-    sendResponse(res, 404, { message: "No project found with given ID" });
-  }
-}
-
 function getTopPerformProjects(req, res) {
+  console.log("getting top projects");
   const topPerformProjects = projects
-    .filter(project => project.revenue > 0)
+    .filter((project) => project.revenue > 0)
     .sort((a, b) => b.revenue - a.revenue)
     .slice(0, 3);
 
@@ -31,8 +24,88 @@ function getTopPerformProjects(req, res) {
   }
 }
 
+async function createProject(req, res) {
+  try {
+    const body = await getPostData(req);
+
+    const project = JSON.parse(body);
+
+    console.log(project);
+
+    projects.push(project);
+
+    res.writeHead(201, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(project));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteProject(req, res) {
+  try {
+    const body = await getPostData(req);
+
+    const b = JSON.parse(body);
+
+    console.log(b);
+
+    // console log the type of b.id
+    console.log(typeof b.id);
+
+    // find index of project with id b.id
+    const index = projects.findIndex((project) => project.id === b.id);
+
+    // pop the project out of the array at index
+    projects.splice(index, 1);
+
+    console.log(index);
+    
+
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    return res.end(JSON.stringify({ message: `Project ${b.id} removed` }));
+
+
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function getCompletedProjects(req, res) {
+  console.log("getting completed projects");
+  const completedProjects = projects.filter((project) => project.isCompleted);
+
+  if (completedProjects.length > 0) {
+    sendResponse(res, 200, completedProjects);
+  } else {
+    sendResponse(res, 404, { message: "No completed projects found" });
+  }
+}
+
+
 module.exports = {
   getAllProjects,
-  getProject,
   getTopPerformProjects,
+  deleteProject,
+  createProject,
 };
+
+function getPostData(req) {
+  return new Promise((resolve, reject) => {
+    try {
+      let body = "";
+
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        resolve(body);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 const http = require("http");
-const port = 5001;
 const ProjectController = require("./controller/ProjectController");
 
+const port = 5001;
+
 const server = http.createServer((req, res) => {
-  // Handle CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  // Set CORS headers to allow cross-origin requests
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight requests (OPTIONS method)
+  if (req.method === "OPTIONS") {
+    // Reply successfully to preflight request
+    res.writeHead(200, {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
+    });
+    res.end();
+    return;
+  }
 
   // Define API routes and methods
   const routes = {
-    '/api/v1/projects': {
-      'GET': ProjectController.getAllProjects,
+    "/api/v1/projects/all": {
+      GET: ProjectController.getAllProjects,
     },
-    '/api/v1/projects/top': {
-      'GET': ProjectController.getTopPerformProjects,
+    "/api/v1/projects/top": {
+      GET: ProjectController.getTopPerformProjects,
     },
-    '/api/v1/projects/': {
-      'GET': ProjectController.getProject,
-    }
+    "/api/v1/projects/save": {
+      POST: ProjectController.createProject,
+    },
+    "/api/v1/projects/delete": {
+      POST: ProjectController.deleteProject,
+    },
   };
 
   // Extract route and method from request
@@ -28,8 +44,8 @@ const server = http.createServer((req, res) => {
   if (route) {
     route(req, res);
   } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'No Routes Found' }));
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "No Routes Found" }));
   }
 });
 
